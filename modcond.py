dc18728179ff26e9bcf32d528f4bc5952215c472
BBTVN <cauribeva@unal.edu.co>
2020-01-14 18:02:33 -0500

    Modificacion 3

--- modcond.py
+++ modcond.py
@@ -2,12 +2,11 @@ import numpy as np
 import scipy
 import scipy.integrate as integrate
 import math
+
 #Método de resistencias
 #Se irán haciendo las resistencias desde el exterior de la gota hasta el sustrato
-
+def R_dif_Wrikamaniyake (T_amb, T_int, t_c, h_fg):
 #----------------Resistencia a la difusión del vapor en la capa de no condensables----------------
-
-R_dif=(T_amb-T_int)/(m_p*h_fg)
 
 #R_dif=resistencia térmica por la capa de difusión
 #T_amb=Temperatura ambiente
@@ -15,29 +14,34 @@ R_dif=(T_amb-T_int)/(m_p*h_fg)
 #t_c=Tasa de condensación. Hallada experimentalmente
 #h_fg= Calor latente de condensación
 
+    R_dif=(T_amb-T_int)/(t_c*h_fg)
+    return R_dif
+
+def R_cond_aire_Wrikamanayake (K_aire, delta, S_r):
 #--------------Resistencia a la conducción del aire---------------------
-
-R_cond_aire=delta/(k_aire*S_r)
-
 # K_aire=Conductividad térmica del K_aire
 #delta= grosor de la capa límite
 #S_r=Área proyectada de la gota
 
-#-------Igualdad de la tasa de condensación a la de difusión de masa para hallar la aproximación de delta-----
+    R_cond_aire=delta/(k_aire*S_r)
+    return R_cond_aire
 
-t_c=-D*M*c*((x_i-x_a)/delta)
+def Capa_limite (h_fg, R, T_int, T_boil,D, M, c, x_i, x_a, Delta):
+#------Inicialmente, Ecuación Clausius-Clapeyron para determinar x_i, termino necesario------------------
+    #T_boil=Punto de fusión a presión atmosférica (P_atm)
+    #R= 
 
-#D= coeficiente de difusión del vapor de agua en el aire
-#M=peso molecular del agua
-#x_i= Fraccion molar en la interfase líquido-vapor de la gota determinada por la ecuación de Clausius-Clapeyron
-#x_a=Fracción molar en el agua, determinada por las condiciones ambiente
+    x_i=math.exp(-(h_fg/R)*((1/T_int)-(1/T_boil)))
 
-#------Ecuación Clausius-Clapeyron para determinar x_i------------------
+#-------Igualdad de la tasa de condensación a la de difusión de masa para hallar la aproximación de -----
+    #D= coeficiente de difusión del vapor de agua en el aire
+    #M= peso molecular del agua
+    #x_i= Fraccion molar en la interfase líquido-vapor de la gota determinada por la ecuación de Clausius-Clapeyron
+    #x_a=Fracción molar en el agua, determinada por las condiciones ambiente
+    #c= concentracion
 
-x_i=math.exp(-(h_fg/R)*((1/T_int)-(1/T_boil)))
-
-#T_boil=Punto de fusión a presión atmosférica (P_atm)
-#
+    delta=-D*M*c*((x_i-x_a)/t_c
+    return delta
 
 #--------Resistencia de Interfase líquido-gas----------------------------
 
