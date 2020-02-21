3de832fd13b096edef2733ead12614fe4ac56d9b
camilouribev <cauribeva@unal.edu.co>
2020-02-21 14:50:57 -0500

    Estimación delta

--- modcond.py
+++ modcond.py
@@ -1,99 +1,30 @@
+## Importar librerías 
 
+import matplotlib.pyplot as plt
 import math
 import numpy as np
 import scipy
 import scipy.integrate as integrate
+from functools import partial
 
 
-#--------------------Nomenclatura---------------------------------
-#R_dif=resistencia térmica por la capa de difusión
 
-#T_amb=Temperatura ambiente [K]
-
-#T_int=Temperatura de la gota [K]
-
-#t_c=Tasa de condensación. Hallada experimentalmente [Kg/s]
-
-#h_fg= Calor latente de condensación [J/kg]
-
-# K_aire=Conductividad térmica del K_aire [W/m K]
-
-#delta= grosor de la capa límite [m]
-
-#S_r=Área proyectada de la gota [m²]
-
-#R_g: Constante específica de los gases J/[Kg K]
-
-#v_g: Volumen especifico del vapor de agua [m²/Kg]
-#h_fg: Calor latente de vaporización [J/Kg]
-
-#alfa=Coeficiente de acomodación o condensación;
-# fracción de moléculas de vapor que se mueven en la gota líquida durante el cambio de fase
-#debe ser de entre 0.02 a 0.04 para agua. En Wrikamanayake et al. se usó 0.02
-
-#T_s=Temperatura de superficie de pared [K]
-
-
-#r=radio de la gota [m]
-
-#theta=Ángulo de contacto [°]
-
-T_amb=294.1
-T_int=294.1
-t_c=0.001
-h_fg=
-K_aire=
-delta=
-S_r=
-R_g=
-v_g=
-h_fg=
-alfa=
-T_s=
-theta=
-
-#Método de resistencias
-#Se irán haciendo las resistencias desde el exterior de la gota hasta el sustrato
 def R_dif_Wrikamanayake (T_amb, T_int, t_c, h_fg):
 #----------------Resistencia a la difusión del vapor en la capa de no condensables [K/W]----------------
     R_dif=(T_amb-T_int)/(t_c*h_fg)
     return R_dif
 
-def R_cond_aire_Wrikamanayake (K_aire, delta, S_r):
+def R_cond_aire_Wrikamanayake (r,k_aire, delta):
 #--------------Resistencia a la conducción del aire [K/W]---------------------
-    R_cond_aire=delta/(k_aire*S_r)
+    R_cond_aire=delta/(k_aire*math.pi*(r**2))
     return R_cond_aire
-
-#------------------SECCIÓN PENDIENTE------------------------------
-
-# def Capa_limite (h_fg, R, T_int, T_boil, RH,P_sat, P_amb, D, M, c, x_i, x_a, Delta):
-# #------Inicialmente, Ecuación Clausius-Clapeyron para determinar x_i, termino necesario------------------
-#     #T_boil=Punto de fusión a presión atmosférica (P_atm)
-#     #R=
-#
-#     x_i=math.exp(-(h_fg/R)*((1/T_int)-(1/T_boil)))
-#     #x_a=Fracción molar en el agua, determinada por las condiciones ambientales
-#
-#     x_a=RH**(P_sat/P_amb)
-# #-------Igualdad de la tasa de condensación a la de difusión de masa para hallar la aproximación de -----
-#     #D= coeficiente de difusión del vapor de agua en el aire
-#     #M= peso molecular del agua
-#     #x_i= Fraccion molar en la interfase líquido-vapor de la gota determinada por la ecuación de Clausius-Clapeyron
-#
-#     #c= concentracion
-#
-#     delta=-D*M*c*((x_i-x_a)/t_c
-#     return delta
-
-#----------------FIN DE SECCIÓN PENDIENTE-----------------------
-
-
-def R_int_liq_Wrikamanayake (r, h_i, theta, alfa, T_s):
-
 #--Definición de h_i (coeficiente interfacial)
 #h_i=Coeficiente interfacial de transferencia de Calor
 
-    h_i=((2*alfa)/(2-alfa))*(1/math.sqrt(2*math.pi*R_g*T_s))*((h_fg**2)/v_g*T_s)
+h_i=((2*alfa)/(2-alfa))*(1/math.sqrt(2*math.pi*R_g*T_sat))*((h_fg**2)/(v_g*T_s))# Calcular de otra manera
+
+def R_int_liq_Wrikamanayake (r, theta, alfa, T_s):
+
 
 #--------Resistencia de Interfase líquido-gas [K/W]----------------------------
 
@@ -101,37 +32,105 @@ def R_int_liq_Wrikamanayake (r, h_i, theta, alfa, T_s):
 
     return R_int
 
-def R_cond_gotas_Wrikamanayake(theta, k1, r):
-#-----Resistencia a la conducción de las gotas [K/W]-------
+def R_cond_gotas_Wrikamanayake(r,theta, k1):
+    """"Resistencia a la conducción de las gotas [K/W]"""
 
     R_drop=theta/(4*math.pi*k1*r*math.sin(theta))
 
     return R_drop
 
+def Q_total_n(r,R_dif,T_amb,T_s):
+ "Resistencia total por gotas pequeñas"""
+ Q_total_n=((T_amb-T_s)/(((1/R_dif+(1/R_cond_dif(r)))**(-1))+R_int_liq(r)+R_got(r)))*n(r)
+ return Q_total_n
+    
+def Q_total_N(r,R_dif,T_amb,T_s,R_cond_dif,R_int_liq,R_got):
+#Resistencia totala por gotas grandes'''
+ Q_total_N = ((T_amb-T_s)/(((1/R_dif+(1/R_cond_dif(r)))**(-1))+R_int_liq(r)+R_got(r)))*N(r)
+ return Q_total_N
+
+
+def N_LeFevre(r, r_max):
+ """ Distribución de tamaño de gotas para gotas grandes """
+ N = 1/(3*math.pi*r**2*r_max) * (r/r_max)**(-2/3)
+ return N
+
+    
+def r_e_KimKim(N_s):
+ """ Radio efectivo de la gota """
+ r_e = (4*N_s)**(-0.5)
+ return r_e
+
+r_e=r_e_KimKim(N_s)
+    
+def n_KimKim(r, deltaT_sub, r_min, delta_coat, k_coat, k_c, theta, h_i, rho, h_fg, r_e, r_max):
+ """ Distribución de tamaño de gotas para gotas pequeñas """
+ A_1 = deltaT_sub / (2*rho*h_fg)
+ A_2 = theta * (1-math.cos(theta)) / (4*k_c*math.sin(theta))
+ A_3 = 1/(2*h_i) + delta_coat*(1-math.cos(theta)) / (k_coat*(math.sin(theta))**2)
+ tau = 3*r_e**2 * (A_2*r_e + A_3)**2 / (A_1*(11*A_2*r_e**2 - 14*A_2*r_e*r_min + 8*A_3*r_e - 11*A_3*r_min))
+ B_1 = A_2/(tau*A_1) * ((r_e**2-r**2)/2 + r_min*(r_e-r) - r_min**2*math.log((r-r_min)/(r_e-r_min))) 
+ B_2 = A_3/(tau*A_1) * (r_e-r - r_min*math.log((r-r_min)/(r_e-r_min)))
+ n = 1/(3*math.pi*r_e**3*r_max) * (r_e/r_max)**(-2/3) * r*(r_e-r_min)/(r-r_min) * (A_2*r+A_3)/(A_2*r_e+A_3) * math.exp(B_1+B_2)
+ return n 
+
+
 #-----------RESISTENCIA TÉRMICA TOTAL [K/W]---------------
+
 R_dif=R_dif_Wrikamanayake(T_amb, T_int ,t_c, h_fg)
-R_cond_aire=R_cond_aire_Wrikamanayake(K_aire, delta, S_r)
-R_int= R_int_liq_Wrikamanayake (r, h_i, theta, alfa, T_s)
-R_drop=R_cond_gotas_Wrikamanayake(theta, k1, r)
-
-R_total=(((1/R_dif)+(1/R_cond_aire))**(-1))+R_int+R_drop
-
-#-----TRANSFERENCIA DE CALOR DEPENDIENTE DEL RADIO DE LA GOTA----------
-
-q=(T_amb-T_s)/R_total
+R_cond_aire=R_cond_aire_Wrikamanayake(r,k_aire, delta)
+R_int= R_int_liq_Wrikamanayake (r, theta, alfa, T_s)
+R_drop=R_cond_gotas_Wrikamanayake(r, theta, k1)
 
 
 
+deltaT_sub=T_amb-T_s
+# Definir funciones para la tasa de transferencia de calor
 
-print R_total
 
-# #-----FLUJO DE CALOR EN LA SUPERFICIE CONOCIENDO LA DISTRIBUCIÓN DE GOTAS
-#
-# from scipy.integrate import quad
-#
-# def f(r):
-#     return q(r)*N(r) # está incompleta
-#
-# i, err= quad(f, r_min, r_max)
-# print (i)
-# print (err)
+
+R_cond_dif = partial(R_cond_aire_Wrikamanayake, k_aire=k_aire, delta=delta)
+R_cond_dif.__doc__ = "Resistencia para conducción del aire  en cada gota dependiente del radio r en m"  
+   
+R_int_liq = partial(R_int_liq_Wrikamanayake, theta=theta, alfa=alfa, T_s=T_s)
+R_int_liq.__doc__ = "Resistencia para interfase líquida dependiente del radio en m" 
+    
+    
+R_got= partial(R_cond_gotas_Wrikamanayake,theta=theta, k1=k1)
+R_got.__doc__ = "Resistencia conducción en las gotas, dependiente del r en m" 
+    
+    
+
+n = partial(n_KimKim, deltaT_sub=deltaT_sub, r_min=r_min, delta_coat=delta_coat, k_coat=k_coat, k_c=k_c,
+                theta=theta, h_i=h_i, rho=rho, h_fg=h_fg, r_e=r_e, r_max=r_max)
+n.__doc__ = "drop size distribution for small drops depending on drop radius r in m"
+N = partial(N_LeFevre, r_max=r_max)
+N.__doc__ = "drop size distribution for large drops depending on drop radius r in m"
+
+    # integrate and calculate heat flux density
+    
+
+"""Integración de los calores de acuerdo al número de gotas"""
+
+    
+q_n, q_n_interr = integrate.quad(Q_total_n, r_min, r_max,(R_dif, T_amb, T_s))
+q_N, q_N_interr = integrate.quad(Q_total_N, r_min, r_max,( R_dif, T_amb, T_s, R_cond_dif, R_int_liq, R_got))
+    
+    
+q = q_n + q_N
+
+    
+print("R_dif:", R_dif,"\n"
+"R_cond_aire:",R_cond_aire,"\n"
+      "R_int:", R_int,"\n"
+      "R_drop:",R_drop,"\n"
+      "R_dif:",R_dif,"\n"
+      "T_amb:",T_amb,"\n"
+      "q_N:",q_N,"W/m2\n"
+      "q_n",q_n,"W/m2\n"
+      "q:", round(q,2),"W/m2\n"
+        "Relacion q_N/q:",q_N/q,"\n")
+
+print ("h_i:", round(h_i,1), "\n")
+
+
